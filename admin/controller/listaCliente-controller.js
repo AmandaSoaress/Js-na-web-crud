fb1a7e208c7cee2c53c29314fa8bb10cc55c9542
Amanda Santos <113608404+AmandaSoaress@users.noreply.github.com>
2023-06-19 13:24:25 -0300

    atualiza

--- admin/controller/listaCliente-controller.js
+++ admin/controller/listaCliente-controller.js
@@ -8,7 +8,7 @@ const criaNovaLinha = (nome, email, id) => {
 <td>${email}</td>
 <td>
    <ul class="tabela__botoes-controle">
-       <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
+       <li><a href="../telas/edita_cliente.html?id=${id}" class="botao-simples botao-simples--editar">Editar</a></li>
        <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
    </ul>
 </td> `
@@ -21,23 +21,34 @@ console.log(linhaNovoCliente)
 const tabela = document.querySelector('[data-tabela]')
 //percorre a arvore do dom
 
-tabela.addEventListener('click', (evento) => {
+tabela.addEventListener('click', async (evento) => {
     let ehBotaoDeletar = evento.target.className === 'botao-simples botao-simples--excluir'
     if (ehBotaoDeletar) {
-        const linhaCliente = evento.target.closest('[data-id]')
-        let id = linhaCliente.dataset.id
-        clienteService.removeCliente(id)
-            .then(() => {
+        try {
+            const linhaCliente = evento.target.closest('[data-id]')
+            let id = linhaCliente.dataset.id
+            await clienteService.removeCliente(id)
                 linhaCliente.remove()
-            })
+        } catch (erro) {
+            console.log(erro)
+            window.location.href='../telas/erro.html'
+        }
+        
     }
 })
-
-clienteService.listaClientes()
-    .then(data => {
-        data.forEach(elemento => {
+const render  = async () => {
+    try {  const listaClientes =  await clienteService.listaClientes()
+    listaClientes.forEach(elemento => {
             tabela.appendChild(criaNovaLinha(elemento.nome, elemento.email, elemento.id))
-
         })
-    })
+        
+    } catch (erro) {
+        console.log(erro)
+        window.location.href='../telas/erro.html'
+
+    }
+  
+}
+render()
+
 //percorre a lista clientes que é uma promessa e faz toda a logica para exibir o dado na tela.
